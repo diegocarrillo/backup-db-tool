07c8b90eb8ba1ee5e71c88b7ef8ab1a664385145
DiegoCarrilloTR <diego.carrillo@thomsonreuters.com>
2020-03-17 18:34:58 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='pgbackup',
+    version='0.1.0',
+    author='Diego Carrillo',
+    autor_email='diegocarrillo.cr@gmail.com',
+    description='A utility for backup up postgreSQL databases',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/diegocarrillo/pgbackup',
+    packages=find_packages('src')
+)
